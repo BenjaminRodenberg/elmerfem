049ea06eda8e1302076b72f64eb5c79d142e71fb
Benjamin Rodenberg <rodenberg@dkrz.de>
2026-01-28 17:57:25 +0100

    Add missing USE Messages ... that causes error in CI.

--- fem/src/modules/UMATLib.F90
+++ fem/src/modules/UMATLib.F90
@@ -16,6 +16,7 @@
        kstep, kinc)
 !------------------------------------------------------------------------------
     USE Types
+    USE Messages, ONLY: Fatal
     IMPLICIT NONE
 
     REAL(KIND=dp), INTENT(INOUT) :: STRESS(NTENS)
@@ -152,6 +153,7 @@
        kstep, kinc)
 !------------------------------------------------------------------------------
     USE Types
+    USE Messages, ONLY: Fatal
     IMPLICIT NONE
 
     REAL(KIND=dp), INTENT(INOUT) :: STRESS(NTENS)
@@ -315,6 +317,7 @@
        kstep, kinc)
 !------------------------------------------------------------------------------
     USE Types
+    USE Messages, ONLY: Fatal
     IMPLICIT NONE
 
     REAL(KIND=dp), INTENT(INOUT) :: STRESS(NTENS)
@@ -604,6 +607,7 @@
        kstep, kinc)
 !------------------------------------------------------------------------------
     USE Types
+    USE Messages, ONLY: Fatal
     IMPLICIT NONE
 
     REAL(KIND=dp), INTENT(INOUT) :: STRESS(NTENS)
