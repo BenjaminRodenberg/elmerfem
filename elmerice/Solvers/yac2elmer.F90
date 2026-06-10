f205359d5c7703cbf0727663d8f53c0e02a9d323
Benjamin Rodenberg <rodenberg@dkrz.de>
2026-06-10 12:08:35 +0200

    Add missing renames in yac2elmer.

--- elmerice/Solvers/yac2elmer.F90
+++ elmerice/Solvers/yac2elmer.F90
@@ -370,7 +370,7 @@ SUBROUTINE YAC2Elmer( Model,Solver,dt,TransientSimulation )
         sal_ocePerm(i) = i
       END DO
       CALL DefaultVariableAdd('temp_oce', dofs=1, Perm = t_ocePerm)
-      CALL DefaultVariableAdd('sal_oce', dofs=1, Perm = sal_ocePerm)
+      CALL DefaultVariableAdd('salinity_boundary', dofs=1, Perm = sal_ocePerm)
 
     END IF
 
@@ -419,14 +419,14 @@ SUBROUTINE YAC2Elmer( Model,Solver,dt,TransientSimulation )
       CALL elmer_icon_interface(is_root_rank)
       CALL INFO(SolverName, 'AFTER ELMER ICON-O INTERFACE', Level=3)
       t_oceVar => VariableGet( Mesh % Variables, 'temp_oce' )
-      sal_oceVar => VariableGet( Mesh % Variables, 'sal_oce' )
+      sal_oceVar => VariableGet( Mesh % Variables, 'salinity_boundary' )
       IF ((.NOT.ASSOCIATED(t_oceVar)) .OR. (.NOT.ASSOCIATED(sal_oceVar))) THEN
         CALL FATAL(SolverName,'Elmer variables not associated')
       END IF
       ! write over values for nodes
       DO i=1, Mesh % NumberOfNodes
         t_oceVar % Values(t_oceVar % Perm(i)) = t_oce_field(i,1)
-        sal_oceVar % Values(sal_oceVar % Perm(i)) = salinity_field(i,1)
+        sal_oceVar % Values(sal_oceVar % Perm(i)) = salinity_boundary_field(i,1)
       END DO
       ! TODO: stub implementation for ICON coupling
       ! CALL elmer_icon_interface(is_root_rank)
