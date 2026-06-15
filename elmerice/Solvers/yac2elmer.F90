53494ae7c709a4ce5a03fc007c085f369e245f1f
Benjamin Rodenberg <rodenberg@dkrz.de>
2026-06-15 15:58:56 +0200

    Use internal domain salinity (sal_oce_post_field) in YAC2Elmer
    The Elmer variable sal_oce_post is now populated with the post-creep-mapping
    values covering the full internal domain, instead of the raw boundary-only
    values received from ICON.

--- elmerice/Solvers/yac2elmer.F90
+++ elmerice/Solvers/yac2elmer.F90
@@ -145,7 +145,7 @@ SUBROUTINE YAC2Elmer( Model,Solver,dt,TransientSimulation )
   USE elmer_ebfm_coupling, ONLY: elmer_ebfm_interface, t_ice_field, smb_field, &
                                  runoff_field, surface_height_field
   USE elmer_icon_coupling, ONLY: elmer_icon_interface, t_oce_field, &
-                                 sal_oce_pre_field
+                                 sal_oce_post_field
 
   IMPLICIT NONE
 
@@ -427,7 +427,7 @@ SUBROUTINE YAC2Elmer( Model,Solver,dt,TransientSimulation )
       ! write over values for nodes
       DO i=1, Mesh % NumberOfNodes
         t_oceVar % Values(t_oceVar % Perm(i)) = t_oce_field(i,1)
-        sal_oceVar % Values(sal_oceVar % Perm(i)) = sal_oce_pre_field(i,1)
+        sal_oceVar % Values(sal_oceVar % Perm(i)) = sal_oce_post_field(i,1)
       END DO
       ! TODO: stub implementation for ICON coupling
       ! CALL elmer_icon_interface(is_root_rank)
